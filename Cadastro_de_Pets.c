5982b8e48e0a5c47855942ade53e3b9f9ba4cd9c
guilhermepg <48468261+guilhermepg@users.noreply.github.com>
2019-09-19 10:56:48 -0300

    Update Cadastro_de_Pets.c

--- Cadastro_de_Pets.c
+++ Cadastro_de_Pets.c
@@ -1,17 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/*struct dados_Pet{
+struct no{
 	char nome[12];
 	char especie[20];
 	char raca[10];
 	char sexo;
 	int idade;
-	char carac[10];
-}; */
-
-struct no{
-	int valor;
 	struct no *proximo;
 };
 
