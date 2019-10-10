c538b2d16adb467044c00949739b473fb0c3ffe9
guilhermepg <48468261+guilhermepg@users.noreply.github.com>
2019-10-10 10:45:18 -0300

    Update Cadastro_de_Pets.c
    Added colors to the options

--- Cadastro_de_Pets.c
+++ Cadastro_de_Pets.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <conio.h>
+#include "tela.h"
 
 struct no{
 	char nome[12];
@@ -34,9 +36,13 @@ void abertura(){
 }
 
 void opcoes(){
-	printf("Selecione qual tarefa deseja realizar:\n\n");
+	textcolor(AMARELO);
+	printf("Bem Vindo ao Cadastro de Pets!!!\nSelecione qual tarefa deseja realizar:\n\n");
+	textcolor(AZUL_ESCURO);
 	printf("Inclusao de um novo Pet: [1]\n");
+	textcolor(VERMELHO_ESCURO);
 	printf("Remocao de um Pet: [2]\n");
+	textcolor(AZUL_ESCURO);
 	printf("Alteracao de informacao: [3]\n");
 	printf("Busca de um Pet pelo seu nome: [4]\n");
 	printf("Busca de um Pet por especie: [5]\n");
@@ -46,12 +52,12 @@ void opcoes(){
 	printf("Mostrar a quantidade de Pets de X especie: [9]\n");
 	printf("Listagem de todos os Pets cadastrados: [10]\n");
 	printf("Sair do Cadastro: [11]\n\n");
-	printf("Listagem de todos os Pets cadastrados: [10]\n\n");
 	printf("Digite uma opcao: ");
+	textcolor(BRANCO);
 }
 
 int main(){
-	
+		
 	struct no *primeiroNo;
 	primeiroNo = NULL;
 	
@@ -71,19 +77,30 @@ int main(){
 		switch ( opcao )
 		{
 			case 1:
+					textcolor(AZUL_ESCURO);
 				printf("\nInsira o nome do Pet: ");
+					textcolor(BRANCO);
 				scanf("%s",&n);
-				printf("\nInsira a especie do %s: ",n);
+					textcolor(AZUL_ESCURO);
+				printf("\nInsira a especie do(a) %s: ",n);
+					textcolor(BRANCO);
 				scanf("%s",&e);
-				printf("\nInsira a raca do %s: ",n);
+					textcolor(AZUL_ESCURO);
+				printf("\nInsira a raca do(a) %s: ",n);
+					textcolor(BRANCO);
 				scanf("%s",&r);
-				printf("\nInsira o sexo do %s: ",n);
+					textcolor(AZUL_ESCURO);
+				printf("\nInsira o sexo do(a) %s: ",n);
+					textcolor(BRANCO);
 				scanf("%s",&s);
-				printf("\nInsira a idade do %s: ",n);
+					textcolor(AZUL_ESCURO);
+				printf("\nInsira a idade do(a) %s: ",n);
+					textcolor(BRANCO);
 				scanf("%d",&i);
 				//novoPet(primeiroNo,n,e,r,s,i);
 				
 				opcao = NULL;
+				printf("\n\n");
 				break;
 				
 			case 2:
@@ -131,48 +148,5 @@ int main(){
 	}
 	
 	printf("\nFim do Cadastro de pet\n");
-	abertura();
-	opcoes();
-	
-	int opcao;
-	
-	scanf("%d",&opcao);
-	switch ( opcao )
-	{
-		case 1:
-			break;
-			
-		case 2:
-			break;
-			
-		case 3:
-			break;
-			
-		case 4:
-			break;
-			
-		case 5:
-			break;
-			
-		case 6:
-			break;
-			
-		case 7:
-			break;
-			
-		case 8:
-			break;
-			
-		case 9:
-			break;
-			
-		case 10:
-			break;
-			
-		default :
-			printf("\nOPCAO INVALIDA!!! Escolher outra\n\n");
-	}
-	
-	
 	return 0;
 }
